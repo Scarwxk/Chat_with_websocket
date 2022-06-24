a5430f2d5d20814b8a279398fff3ac35d0f230a0
Louis <louis.schinkler@gmail.com>
2022-06-24 16:25:16 +0200

    project commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+
+io.on('connection', (socket) => {
+    console.log('Un utilisateur s\'est connecté.');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    })
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
+})
+
+
+
+server.listen(3000, () => {
+    console.log("Ecoute sur le port 3000");
+})
